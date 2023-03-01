7e4d8a1042a876b69a90854b392c88671a402d5c
Intira-72 <intira.vinit@gmail.com>
2023-03-01 15:24:31 +0700

    JSX Components Props

--- social-media-website/src/App.js
+++ social-media-website/src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
 
+const Job = (props) => {
+  return (
+    <div>
+      <h1>{props.position}</h1>
+      <p><b>Company :</b>{props.company}</p>
+      <p><b>Salary :</b>{props.salary}</p>
+    </div>
+  );
+};
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello, React</h1>
+      <Job salary={90000} position="Senior SDE" company="Amazon" />
+      <Job salary={12000} position="Junior SDE" company="Google" />
+      <Job salary={10000} position="Project Manager" company="Netflix" />
     </div>
   );
-}
+};
 
 export default App;
