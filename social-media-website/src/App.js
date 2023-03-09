478537e01fa917071b2073b275d52fde49b8e53c
Intira-72 <intira.vinit@gmail.com>
2023-03-09 15:27:20 +0700

    How To Fetch Data From an API in React Exercise

--- social-media-website/src/App.js
+++ social-media-website/src/App.js
@@ -4,21 +4,33 @@ import './App.css';
 
 
 function App() {
-  const [ inputName, setInputName ] = useState("");
-  const [ predicted, setPredicted ] = useState({});
-  const url = `https://api.agify.io/?name=${ inputName }`;
+  const [ excuseData, setExcuseData ] = useState({});  
 
-  const fetchData = () => { 
-    Axios.get(url).then((res) => setPredicted(res.data));
-    return true;
+  const fetchData = (event) => { 
+    const url = `https://excuser-three.vercel.app/v1/excuse/${ event.target.value }/`;
+    console.log(url);
+    Axios.get(url).then((res) => setExcuseData(res.data[0]));
   }
 
   return (
     <div className="App">
-      <input onChange={(event) => { setInputName(event.target.value) }} type="text" placeholder='Type Your Name' />     
-      <button onClick={ fetchData }>Predict Age</button>  
-      <h1>{ predicted?.name }</h1>    
-      <h1>Predicted Age : { predicted?.age }</h1>
+      <h1>Generate An Excuse</h1>
+      <div>
+        <button onClick={ fetchData } value="family">Family</button>
+        <button onClick={ fetchData } value="office">Office</button>
+        <button onClick={ fetchData } value="children">Children</button>
+        <button onClick={ fetchData } value="college">College</button>
+        <button onClick={ fetchData } value="party">Party</button>
+      </div>
+      <div>
+        <button onClick={ fetchData } value="funny">Funny</button>
+        <button onClick={ fetchData } value="unbelievable">Unbelievable</button>
+        <button onClick={ fetchData } value="developers">Developers</button>
+        <button onClick={ fetchData } value="gaming">Gaming</button>
+      </div>
+      
+      <h3>{ excuseData?.category }</h3>
+      <p>{ excuseData?.excuse }</p>
     </div>
   );
 };
